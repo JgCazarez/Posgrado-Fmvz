def2a770a0dcf30a6b5155c740af454bea12648f
GabrielCazarez21@info.uas.edu.mx <jg.cazarez21@info.uas.edu.mx>
2026-08-06 03:20:01 -0700

    check point navbar

--- js/site-header.js
+++ js/site-header.js
@@ -15,13 +15,13 @@ class SiteHeader extends HTMLElement {
         <div class="nav-container">
           <ul class="nav-list" id="navList">
             <li><a href="${root}index.html">Inicio</a></li>
-            <li class="dropdown"><a href="${root}pages/nosotros.html">Nosotros ▾</a><ul class="dropdown-content"><li><a href="${root}pages/nosotros.html#mision-vision">Misión y Visión</a></li><li><a href="${root}pages/nosotros.html#historia">Historia</a></li><li><a href="${root}pages/nosotros.html#directorio">Directorio</a></li></ul></li>
-            <li class="dropdown"><a href="${root}pages/investigacion.html">Investigación ▾</a><ul class="dropdown-content"><li><a href="#">Redes de investigación</a></li><li><a href="#">Líneas de investigación</a></li><li><a href="#">Cuerpos académicos</a></li><li><a href="${root}pages/doctorado/tesis-en-proceso.html">DCA: Tesis en proceso</a></li><li><a href="${root}pages/maestria/tesis-en-proceso.html">MCA: Tesis en proceso</a></li><li><a href="#">Productividad académica relevante</a></li></ul></li>
-            <li class="dropdown"><a href="${root}pages/oferta-educativa.html">Oferta Educativa ▾</a><ul class="dropdown-content"><li><a href="${root}pages/doctorado/tesis-en-proceso.html">Doctorado</a></li><li><a href="${root}pages/maestria/tesis-en-proceso.html">Maestría</a></li></ul></li>
+            <li class="dropdown"><a href="${root}pages/nosotros.html">Nosotros</a><ul class="dropdown-content"><li><a href="${root}pages/nosotros.html#mision-vision">Misión y Visión</a></li><li><a href="${root}pages/nosotros.html#historia">Historia</a></li><li><a href="${root}pages/nosotros.html#directorio">Directorio</a></li></ul></li>
+            <li class="dropdown"><a href="${root}pages/investigacion.html">Investigación</a><ul class="dropdown-content"><li><a href="#">Redes de investigación</a></li><li><a href="#">Líneas de investigación</a></li><li><a href="#">Cuerpos académicos</a></li><li><a href="${root}pages/doctorado/tesis-en-proceso.html">DCA: Tesis en proceso</a></li><li><a href="${root}pages/maestria/tesis-en-proceso.html">MCA: Tesis en proceso</a></li><li><a href="#">Productividad académica relevante</a></li></ul></li>
+            <li class="dropdown"><a href="${root}pages/oferta-educativa.html">Oferta Educativa</a><ul class="dropdown-content"><li><a href="${root}pages/doctorado/tesis-en-proceso.html">Doctorado</a></li><li><a href="${root}pages/maestria/tesis-en-proceso.html">Maestría</a></li></ul></li>
             <li><a href="#">Vinculación</a></li>
-            <li class="dropdown"><a href="#">Repositorio de Tesis ▾</a><ul class="dropdown-content"><li><a href="#">Tesis DCA</a></li><li><a href="#">Tesis MCA</a></li></ul></li>
-            <li class="dropdown"><a href="#">Seguimiento de Egresados ▾</a><ul class="dropdown-content"><li><a href="#">Formularios</a></li></ul></li>
-            <li class="dropdown"><a href="#">Estudiantes ▾</a><ul class="dropdown-content"><li><a href="#">Trámites</a></li></ul></li>
+            <li class="dropdown"><a href="#">Repositorio de Tesis</a><ul class="dropdown-content"><li><a href="#">Tesis DCA</a></li><li><a href="#">Tesis MCA</a></li></ul></li>
+            <li class="dropdown"><a href="#">Seguimiento de Egresados</a><ul class="dropdown-content"><li><a href="#">Formularios</a></li></ul></li>
+            <li class="dropdown"><a href="#">Estudiantes</a><ul class="dropdown-content"><li><a href="#">Trámites</a></li></ul></li>
             <li><a href="#">Medios de Verificación PNPC</a></li>
           </ul>
         </div>
