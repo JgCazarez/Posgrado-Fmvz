dd052bfb7757387d2cb0fd31fedd76c496db91d3
JgCazarez <jg.cazarez21@info.uas.edu.mx>
2026-08-20 02:53:25 -0700

    cambios del banner de las tablas, ahora el texto es blanco

--- js/site-header.js
+++ js/site-header.js
@@ -4,50 +4,74 @@ class SiteHeader extends HTMLElement {
     this.innerHTML = `
       <header class="site-header">
         <div class="header-container">
-          <a href="${root}index.html" title="Colegio de Ciencias Agropecuarias - UAS">
-            <img src="${root}assets/image-34.png" alt="Escudo UAS" class="logo hide-mobile">
-          </a>
+          <!-- Logo Izquierdo con fondo blanco institucional -->
+          <div class="logo-box logo-left hide-mobile">
+            <a href="${root}index.html" title="Universidad Autónoma de Sinaloa">
+              <img src="${root}assets/image-34.png" alt="Escudo UAS" class="logo">
+            </a>
+          </div>
+
+          <!-- Título Central -->
           <div class="header-titles">
             <h1>COLEGIO DE CIENCIAS AGROPECUARIAS</h1>
           </div>
+
+          <!-- Botón Menú Móvil -->
           <button class="mobile-btn" id="mobileMenuBtn" aria-label="Abrir menú de navegación">
             <span></span>
             <span></span>
             <span></span>
           </button>
-          <a href="${root}index.html" title="Consolidación Global 2029">
-            <img src="${root}assets/consolidacion2029-2.png" alt="Logo Consolidación 2029" class="logo hide-mobile">
-          </a>
+
+          <!-- Logo Derecho con fondo blanco institucional -->
+          <div class="logo-box logo-right hide-mobile">
+            <a href="${root}index.html" title="Consolidación Global 2029">
+              <img src="${root}assets/consolidacion2029-2.png" alt="UAS Visión 2029" class="logo">
+            </a>
+          </div>
         </div>
-        <h2 class="header-subtitle hide-mobile">UNIVERSIDAD AUTÓNOMA DE SINALOA</h2>
+        
+        <!-- Subtítulo Franja Azul Oscura -->
+        <h2 class="header-subtitle hide-mobile">U N I V E R S I D A D &nbsp; A U T Ó N O M A &nbsp; D E &nbsp; S I N A L O A</h2>
       </header>
 
+      <!-- Barra de Navegación Amarilla Institucional con las 9 Secciones Exactas -->
       <nav class="main-nav" aria-label="Navegación principal">
         <div class="nav-container">
           <ul class="nav-list" id="navList">
-            <li><a href="${root}index.html">Inicio</a></li>
             
+            <!-- 1. Inicio -->
+            <li>
+              <a href="${root}index.html">Inicio</a>
+            </li>
+            
+            <!-- 2. Nosotros -->
             <li class="dropdown">
               <a href="${root}pages/nosotros.html">Nosotros</a>
               <ul class="dropdown-content">
                 <li><a href="${root}pages/nosotros.html#bienvenida">Mensaje de Bienvenida</a></li>
-                <li><a href="${root}pages/nosotros.html#dca-mision">DCA: Objetivo, Misión y Visión</a></li>
-                <li><a href="${root}pages/nosotros.html#mca-mision">MCA: Objetivo, Misión y Visión</a></li>
+                <li><a href="${root}pages/nosotros.html#dca-mision">DCA: Objetivo, Misión Visión y Metas</a></li>
+                <li><a href="${root}pages/nosotros.html#mca-mision">MCA: Objetivo, Misión Visión y Metas</a></li>
+                <li><a href="${root}pages/nosotros.html#normatividad">Normatividad</a></li>
+                <li><a href="${root}pages/nosotros.html#logos">LOGOS</a></li>
                 <li><a href="${root}pages/nosotros.html#directorio">Directorio</a></li>
               </ul>
             </li>
 
+            <!-- 3. Investigación -->
             <li class="dropdown">
               <a href="${root}pages/investigacion.html">Investigación</a>
               <ul class="dropdown-content">
-                <li><a href="${root}pages/investigacion.html#lgac">Líneas de Investigación (LGAC)</a></li>
-                <li><a href="${root}pages/investigacion.html#cuerpos">Cuerpos Académicos</a></li>
-                <li><a href="${root}pages/investigacion.html#redes">Redes de Investigación</a></li>
-                <li><a href="${root}pages/doctorado/tesis-en-proceso.html">DCA: Tesis en Proceso</a></li>
-                <li><a href="${root}pages/maestria/tesis-en-proceso.html">MCA: Tesis en Proceso</a></li>
+                <li><a href="${root}pages/investigacion.html#redes">Redes de investigación</a></li>
+                <li><a href="${root}pages/investigacion.html#lgac">Líneas de investigación</a></li>
+                <li><a href="${root}pages/investigacion.html#cuerpos">Cuerpos académicos</a></li>
+                <li><a href="${root}pages/doctorado/tesis-en-proceso.html">DCA: Tesis en proceso</a></li>
+                <li><a href="${root}pages/maestria/tesis-en-proceso.html">MCA: Tesis en proceso</a></li>
+                <li><a href="${root}pages/investigacion.html#productividad">Productividad académica relevante</a></li>
               </ul>
             </li>
 
+            <!-- 4. Oferta Educativa (Mega Menú) -->
             <li class="dropdown">
               <a href="${root}pages/oferta-educativa.html">Oferta Educativa</a>
               <ul class="dropdown-content mega-menu">
@@ -55,41 +79,72 @@ class SiteHeader extends HTMLElement {
                   <div class="dropdown-column">
                     <h3>DOCTORADO EN CIENCIAS AGROPECUARIAS</h3>
                     <ul>
-                      <li><a href="${root}pages/oferta-educativa.html#dca-general">Información General</a></li>
-                      <li><a href="${root}pages/oferta-educativa.html#dca-plan">Plan de Estudios</a></li>
-                      <li><a href="${root}pages/oferta-educativa.html#dca-perfiles">Perfiles de Ingreso / Egreso</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#dca-general">General</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#dca-plan">Plan de Estudios Doctorado</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#dca-perfiles">Perfiles</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#dca-nab">Docentes</a></li>
                       <li><a href="${root}pages/oferta-educativa.html#dca-nab">Núcleo Académico Básico</a></li>
-                      <li><a href="${root}pages/doctorado/tesis-en-proceso.html">Tesis DCA en Proceso</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#egresados">Egresados</a></li>
                     </ul>
                   </div>
                   <div class="dropdown-column">
                     <h3>MAESTRÍA EN CIENCIAS AGROPECUARIAS</h3>
                     <ul>
-                      <li><a href="${root}pages/oferta-educativa.html#mca-general">Información General</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#mca-general">General</a></li>
                       <li><a href="${root}pages/oferta-educativa.html#mca-plan">Plan de Estudios</a></li>
-                      <li><a href="${root}pages/oferta-educativa.html#mca-perfiles">Perfiles de Ingreso / Egreso</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#mca-perfiles">Perfiles</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#mca-nab">Docentes</a></li>
                       <li><a href="${root}pages/oferta-educativa.html#mca-nab">Núcleo Académico Básico</a></li>
-                      <li><a href="${root}pages/maestria/tesis-en-proceso.html">Tesis MCA en Proceso</a></li>
+                      <li><a href="${root}pages/oferta-educativa.html#egresados">Egresados</a></li>
                     </ul>
                   </div>
                 </div>
               </ul>
             </li>
 
+            <!-- 5. Vinculación -->
+            <li>
+              <a href="${root}pages/investigacion.html#redes">Vinculación</a>
+            </li>
+
+            <!-- 6. Repositorio de Tesis -->
             <li class="dropdown">
-              <a href="#">Tesis y Egresados</a>
+              <a href="#">Repositorio de Tesis</a>
               <ul class="dropdown-content">
-                <li><a href="${root}pages/doctorado/tesis-en-proceso.html">DCA: Tesis en Proceso</a></li>
-                <li><a href="${root}pages/maestria/tesis-en-proceso.html">MCA: Tesis en Proceso</a></li>
-                <li><a href="${root}pages/oferta-educativa.html#egresados">Seguimiento de Egresados</a></li>
+                <li><a href="${root}pages/maestria/tesis-en-proceso.html">Maestría en Ciencias Agropecuarias</a></li>
+                <li><a href="${root}pages/doctorado/tesis-en-proceso.html">Doctorado en Ciencias Agropecuarias</a></li>
               </ul>
             </li>
 
+            <!-- 7. Seguimiento de Egresados -->
+            <li class="dropdown">
+              <a href="#">Seguimiento de Egresados</a>
+              <ul class="dropdown-content">
+                <li><a href="${root}pages/oferta-educativa.html#egresados">MCA: Seguimiento de Egresados</a></li>
+                <li><a href="${root}pages/oferta-educativa.html#egresados">DCA: Seguimiento de Egresados</a></li>
+              </ul>
+            </li>
+
+            <!-- 8. Estudiantes -->
+            <li class="dropdown">
+              <a href="#">Estudiantes</a>
+              <ul class="dropdown-content">
+                <li><a href="${root}index.html#convocatorias">Convocatorias</a></li>
+                <li><a href="${root}pages/maestria/tesis-en-proceso.html">MCA: Estudiantes Matriculados</a></li>
+                <li><a href="${root}pages/doctorado/tesis-en-proceso.html">DCA: Estudiantes Matriculados</a></li>
+                <li><a href="${root}pages/nosotros.html#normatividad">MCA: Procesos Administrativos</a></li>
+                <li><a href="${root}pages/nosotros.html#normatividad">DCA: Procesos Administrativos</a></li>
+                <li><a href="${root}pages/nosotros.html#normatividad">Formatos</a></li>
+              </ul>
+            </li>
+
+            <!-- 9. Medios de Verificación PNPC -->
             <li>
               <a href="https://aplicaciones.conacyt.mx:7443/generador-view-angular/index.html?application=PNPC#/login" target="_blank" rel="noopener noreferrer">
-                Medios de Verificación PNPC ↗
+                Medios de Verificación PNPC
               </a>
             </li>
+
           </ul>
         </div>
       </nav>
